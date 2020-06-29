437c5b71c4bceec9d06df107b5b40905381cb5e1
lhernandez99ut <51166488+lhernandez98@users.noreply.github.com>
2020-06-28 20:26:39 -0500

    Cambio #1
    Changes Button & Text View

--- app/src/main/java/com/utrm/app/MainActivity.java
+++ app/src/main/java/com/utrm/app/MainActivity.java
@@ -1,14 +1,37 @@
 package com.utrm.app;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //VARIBALES
+    boolean buttonStatus;
+    Button boton;
+    TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //START
+        buttonStatus = true;
+        boton = findViewById(R.id.button1);
+        text = findViewById(R.id.textView1);
+    }
+
+
+    //SET THE METHODS
+    public void clickbutton(View view){
+        if(buttonStatus == true){
+            text.setText("Hello, this is a button");
+            buttonStatus = false;
+        }else{
+            text.setText("The button has changed!");
+            buttonStatus = true;
+        }
     }
 }
